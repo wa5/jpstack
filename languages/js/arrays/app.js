a026d7c77137723da92758b2e1082f12b2a6b320
wa5 <waseemahmed116@gmail.com>
2021-07-17 09:26:19 +0530

    ggfgf

--- languages/js/arrays/app.js
+++ languages/js/arrays/app.js
@@ -1,7 +1,14 @@
 const express=require('express')
 const app=express()
+
 app.get("/",(req,resp)=>{
     resp.send("hello world")
+})
+app.get("/about",(req,resp)=>{
+    resp.send("hello world about")
+})
+app.get("/contact",(req,resp)=>{
+    resp.send("hello world contact")
 })
 app.listen(8000,()=>{
     console.log("im working")
