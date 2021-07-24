107ca9a5b10cd0cd82c73ddcd6f32b5725eac215
wa5 <waseemahmed116@gmail.com>
2021-07-24 09:27:26 +0530

    fgf
    fdf

--- languages/js/ecma6/modules/static/akshita-gives-gits.js
+++ languages/js/ecma6/modules/static/akshita-gives-gits.js
@@ -1,7 +1,59 @@
+
+/**
+ * !variable named import exportr examples
+ */
+
+
 var name="akshita";
 var gift="chocolate-silk"
 
-//diffult export and named export
-
 export default name;
 export {gift};
+//diffult export and named export
+
+/**
+ * !function default  exportr examples
+ */
+ export default function eat(){
+    console.log("i will have jamoons yammmy")
+}
+
+/**
+ * !function named  exportr examples
+ */
+export function games(){
+    console.log(" i want to play antakshiri")
+}
+
+/**
+ * !export ann arry
+ */
+
+var arry=[66,89,9,0,84]
+export default arry;
+export {arry}
+
+/**
+ * !export ann classs
+ */
+
+
+export class dance {
+
+    lungidance(){
+        console.log("lungi dance lungi dance lungi dance ")
+    }
+}
+
+
+
+export  default class dance1 {
+
+    lungidance(){
+        console.log("lungi dance lungi dance lungi dance ")
+    }
+}
+
+var obj=new dance()
+
+
--- languages/js/ecma6/modules/static/paravalika-taking-gifts.js
+++ languages/js/ecma6/modules/static/paravalika-taking-gifts.js
@@ -1,9 +1,12 @@
+//static import of variables
 import name from './akshita-gives-gits.js'//default import
 import {gift} from './akshita-gives-gits.js'//nmaed import
 import name1 from './umers-gives-gift.js'//default import
 import {gift1} from './umers-gives-gift.js'//nmaed import
 import name2 from './vinodth-gives-gift.js'//default import
 import {gift2} from './vinodth-gives-gift.js'//nmaed import
+
+import dance from './akshita-gives-gits.js'
 
 
 
@@ -16,3 +19,5 @@ function call(){
     console.log("im so happy becuse  "+name2+"  gave me ",gift2)
 }
 call()
+var obj=new dance()
+obj.lungidance()
